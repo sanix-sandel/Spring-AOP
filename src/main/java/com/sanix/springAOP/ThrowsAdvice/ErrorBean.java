217756661f47d4df4a08f86d07d8b408dd773a5e
sanix <sanicksikani@gmail.com>
2020-11-21 23:58:04 +0300

    Throws Advice

---
+++ src/main/java/com/sanix/springAOP/ThrowsAdvice/ErrorBean.java
@@ -0,0 +1,12 @@
+package com.sanix.springAOP.ThrowsAdvice;
+
+public class ErrorBean {
+
+    public void errorProneMethod() throws Exception{
+        throw new Exception("Generic Exception");
+    }
+
+    public void otherErrorProneMethod() throws IllegalArgumentException{
+        throw new IllegalArgumentException("IllegalArgument Exception");
+    }
+}
